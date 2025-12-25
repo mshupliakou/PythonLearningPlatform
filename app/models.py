a96b1b452b14a8eaaf6fb72d8e9813a74359444f
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-25 12:26:20 +0100

    login manager was added

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
-from . import db
+from . import db, login_manager
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     __tablename__ = 'users'
